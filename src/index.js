2f45aac1d96c2663c45f2f95653199f2cb7eec7e
Georgi Georgiev <georgi.georgiev@live.com>
2019-11-14 11:23:36 +0200

    Moving external dependencies
    Moving external dependencies out of the single file component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+//Import external dependencies
+import $ from 'jquery';
+import 'select2/dist/js/select2.full';
+import 'select2/dist/css/select2.min.css'
+
 // Import vue component
 import component from './Select2.vue';
 
